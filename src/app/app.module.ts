fbbad53004d7666fb33e2ba8f5833d435a6b51c9
Michelle Wong <michellekwong2@gmail.com>
2021-04-26 10:21:21 -0400

    Create FormGroup & createFormControl method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,9 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
@@ -7,15 +8,32 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent implements OnInit {
-
+  registrationForm!: FormGroup;
   dynamicFormArray:any;
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient, private fb:FormBuilder) { }
 
   ngOnInit() {
+    //creating blank formGroup
+    this.registrationForm = this.fb.group({
+      //controls are bind dynamically from api response
+        //so create on method for formControl binding
+
+    });
+
     this.httpClient.get('/assets/DynamicFormControl.json').subscribe(data => {
       this.dynamicFormArray = data;
-      console.log(this.dynamicFormArray)
+      //call method here so once response binded to dynamicFormArray
+        //then bind formControl w/ HTML control
+      this.createFormControl();
+    })
+  }
+
+  createFormControl(){
+    //create formControl based on dynamicFormArray
+      //element = obj w/ id:string
+    this.dynamicFormArray.forEach((element: { ID: string; }) => {
+      this.registrationForm.addControl(element.ID, new FormControl(''))
     })
   }
 
